4f376313d9315a7c661a72c1d92424d5b432812a
Akshay Sharma <akshay.aryan.sharma@gmail.com>
2024-01-18 16:52:44 -0600

    Added extra functionality (decline list, size options, ping role options.

--- lfg.ts
+++ lfg.ts
@@ -9,9 +9,13 @@ import {
   User,
 } from "discord.js";
 
-const statusToEmoji = { available: ":white_check_mark:", tentative: ":grey_question:", decline: ":x:" };
+const statusToEmoji = { available: ":white_check_mark:", tentative: ":grey_question:", decline: "" };
+
+let canPing = true;
 
 export async function lfgResponse(interaction: ChatInputCommandInteraction<CacheType>) {
+  const teamSize = interaction.options.getNumber("size") ?? 5;
+  const rolePing = interaction.options.getRole("ping");
   let players = [
     {
       name: interaction.user.displayName,
@@ -23,9 +27,9 @@ export async function lfgResponse(interaction: ChatInputCommandInteraction<Cache
   const exampleEmbed = new EmbedBuilder()
     .setColor([89, 0, 214])
     .setTitle(`${interaction.user.displayName} is looking for a group!`)
-    .setThumbnail("https://static-00.iconduck.com/assets.00/games-valorant-icon-2048x2048-68la6c9i.png")
+    .setThumbnail("https://vgraphs.com/images/players/cards/valorant-player-looking-for-group-card-avatar.png")
     .addFields({
-      name: `Players in Group (1/5)`,
+      name: `Players in Group (1/${teamSize})`,
       value: players.map((e) => `${statusToEmoji[e.status]} ${e.name}`).join("\n"),
     })
     .setTimestamp();
@@ -57,18 +61,23 @@ export async function lfgResponse(interaction: ChatInputCommandInteraction<Cache
   );
   const pingRow = new ActionRowBuilder<ButtonBuilder>().addComponents(pingButton);
 
-  const response = await interaction.reply({
-    // allowedMentions: {roles: ['801630543477342269']},
-    // content: "<@&801630543477342269>",
+  let reply:any = {
     content: "",
     embeds: [exampleEmbed],
     components: [responseRow, pingRow],
-  });
+  }
+  if (rolePing != null)
+  {
+    reply = {...reply, allowedMentions: {roles: [rolePing.id]}, content: rolePing.toString()};
+  }
+
+  const response = await interaction.reply(reply);
 
   const collector = response.createMessageComponentCollector({ componentType: ComponentType.Button });
 
   collector.on("collect", async (event) => {
     try {
+      let shouldPing = false;
       if (event.customId === "ping") {
         if (!players.filter((e) => e.status !== "decline").length) {
           await event.reply("No players in group.");
@@ -77,7 +86,7 @@ export async function lfgResponse(interaction: ChatInputCommandInteraction<Cache
         pingButton.setDisabled(true);
         pingRow.setComponents(pingButton);
         event.update({ components: [responseRow, pingRow] }).then((test) => {
-          event.followUp(`${createPingList(players)} your group is ready!`);
+          event.followUp(`${createPingList(players, teamSize)} your group is ready!`);
           setTimeout((e) => {
             pingButton.setDisabled(false);
             pingRow.setComponents(pingButton);
@@ -89,6 +98,16 @@ export async function lfgResponse(interaction: ChatInputCommandInteraction<Cache
 
       let player = players.findIndex((e) => e.name === event.user.displayName);
       if (player === -1) {
+        if (event.customId === "available")
+        {
+          if (players.filter((e) => e.status === "available").length === teamSize - 1)
+          {
+            if (canPing)
+            {
+              shouldPing = true;
+            }
+          }
+        }
         players.push({
           name: event.user.displayName,
           status: event.customId,
@@ -102,22 +121,36 @@ export async function lfgResponse(interaction: ChatInputCommandInteraction<Cache
       let availablePlayers = players.filter((e) => e.status === "available").length;
       let fields = [
         {
-          name: `Players in Group (${availablePlayers > 5 ? 5 : availablePlayers}/5)`,
-          value: createPlayerList(players),
+          name: `Players in Group (${availablePlayers > teamSize ? teamSize : availablePlayers}/${teamSize})`,
+          value: createPlayerList(players, teamSize),
         },
       ];
       if (event.customId === "decline")
       {
-        
+        console.log(`${event.user.displayName} declined!`);
       }
-      if (players.filter((e) => e.status !== "decline").length > 5) {
+      if (players.filter((e) => e.status !== "decline").length > teamSize) {
         fields.push({
-          name: `Waitlist`,
-          value: createPlayerList(players, true),
+          name: `Waitlist :hourglass:`,
+          value: createPlayerList(players, teamSize, true),
         });
       }
+      if (players.filter((e) => e.status === "decline").length > 0)
+      {
+        fields.push({name: "Declined :no_entry:", value: createDeclineList(players)})
+      }
       exampleEmbed.setFields(fields);
-      await event.update({ embeds: [exampleEmbed] });
+      if (shouldPing)
+      {
+        canPing = false;
+        event.update({ embeds: [exampleEmbed] }).then((test) => {
+          event.followUp(`${createPingList(players, teamSize)} your group is ready!`);
+          setTimeout((e) => {
+            canPing = true;
+          }, 300000);
+        });
+      }
+      else await event.update({ embeds: [exampleEmbed] });
     } catch (e) {
       console.log("Failed interaction?");
       console.log(e);
@@ -127,20 +160,31 @@ export async function lfgResponse(interaction: ChatInputCommandInteraction<Cache
 
 function createPlayerList(
   players: Array<{ name: string; status: string; user: User; timestamp: number }>,
-  waitlist = false
+  teamSize: number,
+  waitlist = false,
 ) {
   // sort by availability first, timestamp second
   let sortedPlayers = players
     .filter((e) => e.status !== "decline")
     .sort((a, b) => Number(b.status === "available") - Number(a.status === "available") || a.timestamp - b.timestamp);
-  sortedPlayers = waitlist ? sortedPlayers.slice(5) : sortedPlayers.slice(0, 5);
+  sortedPlayers = waitlist ? sortedPlayers.slice(teamSize) : sortedPlayers.slice(0, teamSize);
   return sortedPlayers.length === 0 ? "\u200B" : sortedPlayers.map((e) => `${statusToEmoji[e.status]} ${e.name}`).join("\n");
 }
 
-function createPingList(players: Array<{ name: string; status: string; user: User; timestamp: number }>) {
+function createDeclineList(
+  players: Array<{ name: string; status: string; user: User; timestamp: number }>,
+) {
+  // sort by availability first, timestamp second
+  let sortedPlayers = players
+    .filter((e) => e.status === "decline")
+    .sort((a, b) => a.timestamp - b.timestamp);
+  return sortedPlayers.length === 0 ? "\u200B" : sortedPlayers.map((e) => `${statusToEmoji[e.status]} ${e.name}`).join("\n");
+}
+
+function createPingList(players: Array<{ name: string; status: string; user: User; timestamp: number }>, teamSize: number) {
   let sortedPlayers = players
     .filter((e) => e.status !== "decline")
     .sort((a, b) => Number(b.status === "available") - Number(a.status === "available") || a.timestamp - b.timestamp);
-  sortedPlayers = sortedPlayers.slice(0, 5);
+  sortedPlayers = sortedPlayers.slice(0, teamSize);
   return sortedPlayers.map((e) => e.user).join(", ");
 }
--- slash_commands.ts
+++ slash_commands.ts
@@ -1,21 +1,36 @@
-import { REST, Routes } from "discord.js";
+import { REST, Routes, SlashCommandBuilder } from "discord.js";
 
 const TOKEN = process.env.TOKEN;
 const CLIENT_ID = process.env.CLIENT_ID;
-const rest = new REST({ version: '10' }).setToken(TOKEN);
+const rest = new REST({ version: "10" }).setToken(TOKEN);
 
 const commands = [
-  {
-    name: 'lfg',
-    description: 'Creates a group.',
-    
-  }
+  // {
+  //   name: "lfg",
+  //   description: "Creates a group.",
+  // },
+  new SlashCommandBuilder()
+    .addRoleOption((option) =>
+      option.setName("ping").setDescription("Pings the given role.").setRequired(false)
+    )
+    .addNumberOption((option) =>
+      option
+        .setName("size")
+        .setDescription("The total number of players in the group (including yourself).")
+        .setRequired(false)
+        .setMinValue(2)
+        .setMaxValue(20)
+    )
+    .setName("lfg")
+    .setDescription("Creates a group.")
+    .toJSON()
+    ,
 ];
 export async function refreshCommands() {
   try {
-    console.log('Started refreshing application (/) commands.');
+    console.log("Started refreshing application (/) commands.");
     await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
-    console.log('Successfully reloaded application (/) commands.');
+    console.log("Successfully reloaded application (/) commands.");
   } catch (error) {
     console.error(error);
   }
